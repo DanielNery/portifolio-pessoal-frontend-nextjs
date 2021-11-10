ad0df6503697886b3c3fea4703ef8607abefb435
Daniel Nery <daniel.nery@confitec.com.br>
2021-11-10 14:36:53 -0300

    configuração inicial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,15 +41,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
-
-          <link rel="ico" href="https://rocketseat.com.br/favicon.ico" />
-
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
