49fbc570ccee174a900199933b729327f383bf15
Daniel Nery <daniel.nery@confitec.com.br>
2021-11-18 08:25:37 -0300

    Apresentação pessoal.

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,42 @@
+
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import picture from '../../assets/perfil.png';
+
+function HomeHero() {
+  return (
+    <Container>
+      <img src={picture} alt="minha foto" />
+      <div>
+        <TextContainer>
+          <h1>Olá</h1>
+          <h2>Me chamo Daniel</h2>
+        </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className="comment">//Minha apresentação</span>
+            <span className="purple">Infos</span>{'\u007B'}
+            <div>
+              Nome: <span className="blue">Daniel,</span>
+            </div>
+            <div>
+              Sobrenome: <span className="blue">Nery</span>
+            </div>
+            {'\u007B'}
+          </CodeItem>
+          <CodeItem>
+            <span className="purple">Cargo</span>{'\u007B'}
+            <div>
+              Função: <span className="blue">Dev FullStack</span>
+            </div>
+            <div>
+              Empresa: <span className="blue">Nery</span>
+            </div>
+            {'\u007B'}
+          </CodeItem>
+        </InfosContainer>
+      </div>
+    </Container>
+  );
+};
+
+export default HomeHero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import Header from "../components/Header";
+import HomeHero from "../components/HomeHero";
+import { HomeContainer } from '../styles/HomeStyles';
 
 export default function Home() {
   return (
-    <Header />
+    <HomeContainer>
+        <Header />
+
+        <main className="container"> 
+          <HomeHero />
+        </main>
+
+    </HomeContainer>
   );
 }
