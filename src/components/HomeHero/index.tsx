3773af28e39576f8cdb2a83d21e690269beaf255
dnery <dnery@brasilseg.com.br>
2023-06-02 19:18:37 -0300

    feat: Mudando foto de perfil.

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -6,7 +6,7 @@ function HomeHero() {
   
   return (
     <Container>
-      <Image src="/perfil3.png" alt="me" width="500" height="500" className="perfil" />
+      <Image src="/perfil4.png" alt="me" width="500" height="500" className="perfil" />
       <div>
         <TextContainer>
           <h1>Hello World</h1>
